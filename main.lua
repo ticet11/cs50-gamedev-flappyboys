369fc5d814f18b855b85107ed7af960a468704a2
Brian Kozub <ticet11@gmail.com>
2020-07-06 15:21:19 -0600

    set screen size and resolution.

--- main.lua
+++ main.lua
@@ -1,2 +1,11 @@
 -- visual resolution library
 push = require 'push'
+
+WINDOW_WIDTH = 1280
+WINDOW_HEIGHT = 720
+
+VIRTUAL_WIDTH = 512
+VIRTUAL_HEIGHT = 288
+
+local background = love.graphics.newImage('background.png')
+local ground = love.graphics.newImage('ground.png')
