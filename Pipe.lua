320aed71ce3afabaf5d8fb9ca29a065401406556
Brian Kozub <ticet11@gmail.com>
2020-07-08 10:24:35 -0600

    updated pipe:update to use proper parameters.

--- Pipe.lua
+++ Pipe.lua
@@ -20,10 +20,11 @@ function Pipe:init(orientation, y)
 end
 
 function Pipe:update(dt)
-    self.x = self.x + PIPE_SCROLL * dt
+    self.x = self.x + PIPE_SPEED * dt
 end
 
 function Pipe:render()
-    love.graphics.draw(PIPE_IMAGE, self.x, (self.orientation == 'top' and self.y + PIPE_HEIGHT or self.y), 0, 1,
-                       self.orientation == 'top' and -1 or 1)
+    love.graphics.draw(PIPE_IMAGE, self.x, 
+        (self.orientation == 'top' and self.y + PIPE_HEIGHT or self.y), 0, 1,
+        self.orientation == 'top' and -1 or 1)
 end
