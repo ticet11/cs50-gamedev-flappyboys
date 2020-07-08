7d7f949caf87deb98bd2fd59e161128f693effd4
Brian Kozub <ticet11@gmail.com>
2020-07-08 12:21:53 -0600

    fine-tuned collision points.

--- Bird.lua
+++ Bird.lua
@@ -14,7 +14,7 @@ function Bird:init()
 end
 
 function Bird:collides(pipe)
-    if (self.x + 2) + (self.width - 4) >= pipe.x and self.x + 2 <= pipe.x + PIPE_WIDTH then
+    if (self.x + 2) + (self.width - 35) >= pipe.x and self.x + 2 <= pipe.x + PIPE_WIDTH then
         if (self.y + 2) + (self.height - 4) >= pipe.y and self.y + 2 <= pipe.y + PIPE_HEIGHT then
             return true
         end
