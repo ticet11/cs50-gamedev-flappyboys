f1b728cf1a9dc5ded0324331c0b7ebd4d7930151
Brian Kozub <ticet11@gmail.com>
2020-07-08 10:07:32 -0600

    pipepair class created and used in main.lua.

--- main.lua
+++ main.lua
@@ -7,6 +7,8 @@ require 'Bird'
 
 require 'Pipe'
 
+require 'PipePair'
+
 WINDOW_WIDTH = 1280
 WINDOW_HEIGHT = 720
 
@@ -26,9 +28,11 @@ local BACKGROUND_LOOPING_POINT = 733
 
 local bird = Bird()
 
-local pipes = {}
+local pipePairs = {}
 
 local spawnTimer = 0
+
+local lastY = -PIPE_HEIGHT + math.random(80) + 20
 
 function love.load()
     love.graphics.setDefaultFilter('nearest', 'nearest')
@@ -72,17 +76,22 @@ function love.update(dt)
     spawnTimer = spawnTimer + dt
 
     if spawnTimer > 2 then
-        table.insert(pipes, Pipe())
+        local y = math.max(-PIPE_HEIGHT + 10, math.min(lastY + math.random(-20, 20), VIRTUAL_HEIGHT - 90 - PIPE_HEIGHT))
+        lastY = y
+
+        table.insert(pipePairs, PipePair(y))
         spawnTimer = 0
     end
 
     bird:update(dt)
 
-    for key, pipe in pairs(pipes) do
+    for key, pair in pairs(pipePairs) do
         pipe:update(dt)
+    end
 
-        if pipe.x < -pipe.width then
-            table.remove(pipes, key)
+    for key, pair in pairs(pipePairs) do
+        if pair.remove then
+            table.remove(pipePairs, key)
         end
     end
 
@@ -94,8 +103,8 @@ function love.draw()
 
     love.graphics.draw(background, -backgroundScroll, 0)
 
-    for key, pipe in pairs(pipes) do
-        pipe:render()
+    for key, pair in pairs(pipePairs) do
+        pair:render()
     end
 
     love.graphics.draw(ground, -groundScroll, VIRTUAL_HEIGHT - 16)
