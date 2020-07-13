a6c2eb266634af626bdd3ed1ca8ae6070b6d5f7b
Brian Kozub <ticet11@gmail.com>
2020-07-13 12:39:39 -0600

    music and sound effects added, implemented in main.lua

--- main.lua
+++ main.lua
@@ -56,6 +56,17 @@ function love.load()
     hugeFont = love.graphics.newFont('flappy.ttf', 56)
     love.graphics.setFont(flappyFont)
 
+    sounds = {
+        ['jump'] = love.audio.newSource('jump.wav', 'static'),
+        ['explosion'] = love.audio.newSource('exposion.wav', 'static'),
+        ['hurt'] = love.audio.newSource('hurt.wav', 'static'),
+        ['score'] = love.audio.newSource('score.wav', 'static'),
+        ['music'] = love.audio.newSource('little-happy-tune.wav', 'static')
+    }
+
+    sounds['music']:setLooping(true)
+    sounds['music']:play()
+
     push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
         vsync = true,
         fullscreen = false,
--- readme.md
+++ readme.md
@@ -1,3 +1,6 @@
 # Flappy Boys
 
 A clone of the popular flappy bird. Very cool.
+
+## Music Source
+https://freesound.org/people/cabled_mess/sounds/335361/#comments
