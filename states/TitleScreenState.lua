951c19c4788adef4f79ffd9f03ecfaee54f7236e
Brian Kozub <ticet11@gmail.com>
2020-07-14 13:18:46 -0600

    more specific text on title page.

--- states/TitleScreenState.lua
+++ states/TitleScreenState.lua
@@ -1,5 +1,7 @@
 -- inherits from BaseState
-TitleScreenState = Class{__includes = BaseState}
+TitleScreenState = Class {
+    __includes = BaseState
+}
 
 function TitleScreenState:update(dt)
     if love.keyboard.wasPressed('enter') or love.keyboard.wasPressed('return') then
@@ -12,6 +14,7 @@ function TitleScreenState:render()
     love.graphics.printf('Flappy Boys', 0, 64, VIRTUAL_WIDTH, 'center')
 
     love.graphics.setFont(mediumFont)
-    love.graphics.printf('Press the Spacebar to jump', 0, 128, VIRTUAL_WIDTH, 'center')
+    love.graphics.printf('Press the Spacebar or click to jump', 0, 130, VIRTUAL_WIDTH, 'center')
+    love.graphics.printf('Press P to pause', 0, 150, VIRTUAL_WIDTH, 'center')
     love.graphics.printf('Press Enter to begin', 0, 192, VIRTUAL_WIDTH, 'center')
 end
